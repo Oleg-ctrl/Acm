b6030229cc4d91b615baa601368aab5731cedab0
Oleg B <omblohin1@gmail.com>
2020-06-10 15:43:54 +0300

    2023

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import static java.lang.Math.*;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        int arr[] = new int[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            arr[i] = scanner.nextInt();
+        int size1 = scanner.nextInt();
+        int arr1[] = new int[size1];
+        for (int i = 0; i < size1; i++) {
+            arr1[i] = abs (scanner.nextInt());
         }
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if ((i != j) & (arr[i] % arr[j] == 0)) {
-                    count++;
+
+        int size2 = scanner.nextInt();
+        int arr2[] = new int[size2];
+        for (int i = 0; i < size2; i++) {
+            arr2[i] = Math.abs(scanner.nextInt());
+        }
+        ArrayList <Integer> array = new ArrayList();
+
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++) {
+                if (arr1[i] == arr2[j]) {
+                    array.add(arr1[i]);
+                    break;
                 }
             }
         }
-        System.out.println(count);
+        if (array.size() == 0) {
+            System.out.println(0);
+            System.out.println();
+        } else {
+            System.out.println(array.size());
+            for (int i = 0; i < array.size(); i++) {
+                System.out.print(array.get(i));
+                System.out.println("");
+            }
+
+        }
+
     }
 }
 
